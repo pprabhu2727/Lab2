3a60daed384aab7512e62249e99881ce1a3ba068
ffine <ffine@192.168.0.21>
2021-09-21 12:22:35 -0700

    changed line 14

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,12 +6,12 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("301171663 - This line was added by Prasad Prabhu.");
-		System.out.println("301499367 - This line was added by Frank Fine.");
+		System.out.println("301499367 - This line was added by Rayman Thandi.");
 		System.out.println("219167469 - This line was added by Ronald Her.");
 		System.out.println("301901054 - This line was added by Vincent Boyack.");
 		System.out.println("219502076 - This line was added by Andy Chung.");
 		System.out.println("302484247 - This line was added by Bryan Burch.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("302264079 - This line was added by Frank Fine.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 
